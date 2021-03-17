7108bb4071d2a722cb21381240b92e9a0327db01
Andrew Wallace <andrew.wallace.english@gmail.com>
2021-03-17 17:33:20 +0900

    youtube working but opens multiple windows

--- src/Row.js
+++ src/Row.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import "./Row.css"
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({title, fetchUrl, isLargeRow }) {
     const [movies, setMovies] = useState([]);
-
+    const [trailerUrl, setTrailerUrl] = useState("");
     // code that runs based on variable
     useEffect(() => {
         //if [] run once
@@ -19,6 +21,27 @@ function Row({title, fetchUrl, isLargeRow }) {
     }, [fetchUrl]);
 
     // console.log(movies);
+    const opts = {
+        height: "390",
+        width: "100%",
+        playerVars: {
+            autoplay: 1,
+        },
+    };
+
+    const handleClick = (movie) => {
+        if(trailerUrl) {
+            setTrailerUrl("");
+        } else {
+            movieTrailer(movie?.name || "")
+            .then(url => {
+                // strips url and get youtube video id
+                const urlParams = new URLSearchParams(URL(url).search);
+                setTrailerUrl(urlParams.get(`v`));
+            })
+            .catch(error => console.log(error))
+        }
+    }
 
     return (
         <div className="row">
@@ -30,6 +53,7 @@ function Row({title, fetchUrl, isLargeRow }) {
                 {movies.map(movie => (
                     <img
                         key={movie.id}
+                        onClick={() => handleClick(movie)} 
                         className={`row_poster ${isLargeRow && "row_posterLarge"}`}
                         src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
                         alt={movie.name}
@@ -37,7 +61,7 @@ function Row({title, fetchUrl, isLargeRow }) {
                 ))}
 
             </div>
-
+            <YouTube videoId={trailerUrl} opts={opts} />     
         </div>
     )
 }
